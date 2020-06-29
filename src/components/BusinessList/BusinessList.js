bdd81d3f26901e7705019632f8ab23520849ffa9
sebpalmerio <sebpalmerio@gmail.com>
2020-06-29 18:56:49 -0400

    v0.1

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Business from '../Business/Business'
+import './BusinessList.css';
+
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div className="BusinessList">
+                {
+                    this.props.businesses.map((business) => {
+                        return <Business business={business} />;
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default BusinessList;
